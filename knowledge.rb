6599140175bca18dd1778221a44be20390c8f1ce
Viktor Leonets <4405511@gmail.com>
2013-05-24 17:00:13 +0300

    midway

--- knowledge.rb
+++ knowledge.rb
@@ -2,22 +2,22 @@
 class Object
   def attribute(attr, value = nil)
 
-	    instance_variable_set("@#{attr}", value)
+    instance_variable_set("@#{attr}", value)
 
-	    define_method "#{attr}=" do | value |
-	      instance_variable_set("@#{attr}", value)
-	    end
+    define_method "#{attr}=" do | value |
+      instance_variable_set("@#{attr}", value)
+    end
 
-		define_method "#{attr}?" do
-		    if instance_variable_defined? "@#{attr}"
-		        true
-		    else
-		    	false
-		    end
-	    end
+    define_method "#{attr}?" do
+      if instance_variable_defined? "@#{attr}"
+          true
+      else
+        false
+      end
+    end
 
-	    define_method "#{attr}" do
-	      instance_eval("@#{attr}")
-	    end
+    define_method "#{attr}" do
+      instance_eval("@#{attr}")
+    end
   end
 end
